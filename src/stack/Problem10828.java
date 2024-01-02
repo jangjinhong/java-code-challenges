48aefca2190c3d049d95b5c94d648b290749aed3
jangjinhong <honge1122@naver.com>
2024-01-02 15:03:54 +0900

    [2024-01-02] Refactoring

--- src/stack/Problem10828.java
+++ src/stack/Problem10828.java
@@ -3,47 +3,37 @@ package stack;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class Problem10828 {
     public static void main(String[] args) throws IOException {
-        Stack<Integer> st = new Stack<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        Stack<Integer> st = new Stack<>();
         int n = Integer.parseInt(br.readLine());
-        ArrayList<Integer> status = new ArrayList<>();
+
+        int start = 1;
 
         for(int i=0; i<n; i++) {
-            String[] str = br.readLine().split(" ");
-            switch (str[0]) {
-                case "push" :
-                    st.push(Integer.parseInt(str[1]));
-                    break;
+            int num = Integer.parseInt(br.readLine());
 
-                case "pop" :
-                    if(st.empty())
-                        status.add(-1);
-                    else
-                        status.add(st.pop());
-                    break;
-
-                case "size" :
-                    status.add(st.size());
-                    break;
-
-                case "empty" :
-                    status.add(st.empty()? 1 : 0);
-                    break;
-
-                case "top" :
-                    if(st.empty())
-                        status.add(-1);
-                    else
-                        status.add(st.peek());
-                    break;
+            if(start <= num) {
+                for(int j=start; j<=num; j++) {
+                    st.push(j);
+                    sb.append("+").append("\n");
+                }
+                start = num+1;
             }
+            if(st.peek() != num) {
+                System.out.println("NO");
+                System.exit(0);
+            }
+            st.pop();
+            sb.append("-").append("\n");
+            System.out.println(st);
         }
-        for(int s : status)
-            System.out.println(s);
+        System.out.println(sb);
+
     }
 }
